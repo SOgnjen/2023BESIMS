409d20169bd0cda829f9aaa26862a00324fb5754
Simona Prokic <167simonap@gmail.com>
2022-10-05 15:13:31 +0200

    Update project organization

---
+++ HospitalLibrary/Core/Repository/IRoomRepository.cs
@@ -0,0 +1,18 @@
+﻿using HospitalLibrary.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Core.Repository
+{
+    public interface IRoomRepository
+    {
+        IEnumerable<Room> GetAll();
+        Room GetById(int id);
+        void Create(Room room);
+        void Update(Room room);
+        void Delete(Room room);
+    }
+}
